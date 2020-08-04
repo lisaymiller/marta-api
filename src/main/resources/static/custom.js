956914cb375d4d344b5756042baee9ee8e93565b
Lisa Miller <lisayevonne@outlook.com>
2020-08-04 18:33:52 -0400

    descriptions for functions in js file

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,9 +1,9 @@
-var map;
-
-var image = "bus-marker.png";
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  // image for bus icons
+  let image = "bus-marker.png";
+
+  // making the map with the center being the first bus location
+  let map = new google.maps.Map(document.getElementById("map"), {
     center: {
       lat: parseFloat(busLocations[0].LATITUDE),
       lng: parseFloat(busLocations[0].LONGITUDE),
@@ -12,15 +12,48 @@ function initMap() {
     scrollwheel: false,
   });
 
+  // content inside of the info window
+  let contentString = "${bus.VEHICLE}";
+
+  // info window
+  let infoWindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  // for loop getting bus locations in array
   for (i = 0; i < busLocations.length; i++) {
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
       position: {
         lat: parseFloat(busLocations[i].LATITUDE),
         lng: parseFloat(busLocations[i].LONGITUDE),
       },
       map: map,
+
+      // bounce animation for icons
       animation: google.maps.Animation.DROP,
+
+      // icons using image set from above
       icon: image,
     });
+
+    // animation toggle bounce when clicked
+    marker.addListener("click", toggleBounce);
+
+    // info window open when clicked
+    marker.addListener("click", () => {
+      infoWindow.open(map, marker);
+    });
+
+    // function for toggling bounce animation
+    function toggleBounce() {
+      if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+      } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+    }
+
+    // end of for loop
   }
+  // end of initMap()
 }
